427f455abe2fcd24bce5ba27f809473223ea0a51
Screen <228304862+zaid-gd@users.noreply.github.com>
2026-09-09 22:25:24 +0400

    CF  build falling

--- website/open-next.config.ts
+++ website/open-next.config.ts
@@ -1,3 +1,9 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import {
+  defineCloudflareConfig,
+  type OpenNextConfig,
+} from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default {
+  ...defineCloudflareConfig(),
+  buildCommand: "pnpm run build:next",
+} satisfies OpenNextConfig;
